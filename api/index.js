b22f409fdfb98884e463adb9d51d722fbf1e813a
notgarv01 <garvgupta6778@gmail.com>
2025-09-01 16:04:06 +0530

    2

--- api/index.js
+++ api/index.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
 
-app.set('view engine', 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-    fs.readdir('./files', function(err, files){
-        res.render('index',{files: files})
-    })
-})
-app.get('/file/:filename', function(req,res){
-    fs.readFile(`./files/${req.params.filename}`,'utf-8',function(err,fileData){
-        res.render('show',{filename: req.params.filename, fileData: fileData})
-    })
-})
-app.post('/create', function(req,res){
-    fs.writeFile(`./files/${req.body.title}.txt`,req.body.details,function(err){
-        res.redirect('/')
-    })
-})
+app.get('/', (req, res) => {
+  fs.readdir(path.join(__dirname, '../files'), (err, files) => {
+    if (err) return res.status(500).send('Error reading files');
+    res.render('index', { files });
+  });
+});
 
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(path.join(__dirname, '../files', req.params.filename), 'utf-8', (err, fileData) => {
+    if (err) return res.status(404).send('File not found');
+    res.render('show', { filename: req.params.filename, fileData });
+  });
+});
+
+app.post('/create', (req, res) => {
+  fs.writeFile(path.join(__dirname, '../files', `${req.body.title}.txt`), req.body.details, (err) => {
+    if (err) return res.status(500).send('Error creating file');
+    res.redirect('/');
+  });
+});
+
+// IMPORTANT: No app.listen() here
 module.exports = app;
